f9c536a4fa6f8b7ff23e95eadf8600fc32a5ffd0
Yann Dago <yanndago@hotmail.com>
2015-11-23 18:26:33 -0500

    Classement
    TESTER LE CLASSEMENT

--- Jeu de course/Assets/Cadriciel/Scripts/CheckpointManager.cs
+++ Jeu de course/Assets/Cadriciel/Scripts/CheckpointManager.cs
@@ -3,152 +3,158 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Collections;
 
-public class CheckpointManager : MonoBehaviour 
+public class CheckpointManager : MonoBehaviour
 {
 
-	[SerializeField]
-	private GameObject _carContainer;
-
-	[SerializeField]
-	private int _checkPointCount;
-	[SerializeField]
-	private int _totalLaps;
-
-	private bool _finished = false;
-	private bool _carsSorted = false;
-	
-	private Dictionary<CarController,PositionData> _carPositions = new Dictionary<CarController, PositionData>();
-	List<KeyValuePair<CarController, PositionData>> _carPosLit;
-
-	private class PositionData
-	{
-		public int lap;
-		public int checkPoint;
-		public Vector3 position;
-		public int checkPointCount = 0;
-		public bool startCouting = false;
-	}
-
-	private class CustomComparer : IComparer<KeyValuePair<CarController, PositionData>> {
-		public int Compare(KeyValuePair<CarController, PositionData> car1, KeyValuePair<CarController, PositionData> car2)
-		{
-			//float deltacheckPointCount = Mathf.Sign(car2.Value.checkPointCount - car1.Value.checkPointCount);
-			//return (int)deltacheckPointCount;
-
-			if (car1.Value.checkPointCount < car2.Value.checkPointCount)
-				return 1;
-			else if (car1.Value.checkPointCount > car2.Value.checkPointCount)
-				return -1;
-			return 0;
-		}
-	}
-
-	// Use this for initialization
-	void Awake () 
-	{
-		CarController[] cars = _carContainer.GetComponentsInChildren<CarController> (true);
-		foreach (CarController car in cars)
-		{
-			_carPositions[car] = new PositionData();
-		}
-		_carPosLit = _carPositions.ToList<KeyValuePair<CarController, PositionData>>();
-
-		InvokeRepeating("getRaceRanking",1.0f,2.0f);
-	}
-
-	public void getRaceRanking(){
-
-		_carPosLit = _carPositions.ToList<KeyValuePair<CarController, PositionData>>();
-		_carPosLit.Sort(new CustomComparer());
-		_carsSorted = true;
+    [SerializeField]
+    private  GameObject _carContainer;
 
 
-		int rank = 1;
-		foreach(KeyValuePair<CarController, PositionData> car in _carPosLit){
-			string log = rank.ToString() +  car.Key.ToString() + car.Value.checkPointCount.ToString();
-			//Debug.Log(log);
-			rank++;
-		}
-		rank = 1;
+    public static int SNumberOfCheckPoints;
+    public static Checkpoint[] SCheckPointList;
 
-		////Debug.Log (getFirstCar ().ToString ());
-	}
+    [SerializeField]
+    private int _totalLaps;
 
-	public CarController getFirstCar(){
-		return _carPosLit.First ().Key;
-	}
+    private bool _finished = false;
 
-	public CarController getLastCar(){
-		return _carPosLit.Last ().Key;
-	}
-
-	public CarController getNext(CarController car){
-
-		CarController itemToReturn = null;
-
-		PositionData carPosData = null;
-		carPosData = _carPositions[car];
-		if (carPosData == null)
-			return null;
-
-		KeyValuePair<CarController, PositionData> temp = new KeyValuePair<CarController, PositionData> (car, carPosData);
-		int index = _carPosLit.BinarySearch (temp,new CustomComparer());
-
-		index--;
-		if(index >= 0)
-			itemToReturn = _carPosLit.ElementAt(index).Key;
-
-		for (; index > 0 && index < _carPosLit.Count(); index--) {
-			if(_carPosLit.ElementAt(index).Key && _carPosLit.ElementAt(index).Key.gameObject.activeSelf)
-				return _carPosLit.ElementAt(index).Key;;
-		}
-
-		return itemToReturn;
-	}
-	
-	public void CheckpointTriggered(CarController car, int checkPointIndex)
-	{
-
-		PositionData carData = _carPositions[car];
-		carData.position = car.transform.position;
-
-		if (!_finished)
-		{
-			if (checkPointIndex == 0)
-			{
-				carData.startCouting = true;
-				carData.checkPointCount++;
-
-				if (carData.checkPoint == _checkPointCount-1)
-				{
-					carData.checkPoint = checkPointIndex;
-					carData.lap += 1;
-					//Debug.Log(car.name + " lap " + carData.lap);
-					if (IsPlayer(car))
-					{
-						GetComponent<RaceManager>().Announce("Tour " + (carData.lap+1).ToString());
-					}
-
-					if (carData.lap >= _totalLaps)
-					{
-						_finished = true;
-						GetComponent<RaceManager>().EndRace(car.name.ToLower());
-					}
-				}
-			}
-			else if (carData.checkPoint == checkPointIndex-1) //Checkpoints must be hit in order
-			{
-				carData.checkPoint = checkPointIndex;
-				if(carData.startCouting)
-					carData.checkPointCount++;
-			}
-		}
+    private Dictionary<CarController, PositionData> _carPositions = new Dictionary<CarController, PositionData>();
+    List<KeyValuePair<CarController, PositionData>>  _carPosList;
+    public List<Checkpoint> CheckPointList;
 
 
-	}
+    private class PositionData
+    {
+        public int lap;
+        public int lastCheckPoint;
+        public int position;
+    }
 
-	bool IsPlayer(CarController car)
-	{
-		return car.GetComponent<CarUserControlMP>() != null;
-	}
+    private class CustomComparer : IComparer<KeyValuePair<CarController, PositionData>>
+    {
+        public int Compare(KeyValuePair<CarController, PositionData> car1, KeyValuePair<CarController, PositionData> car2)
+        {
+            //float deltacheckPointCount = Mathf.Sign(car2.Value.checkPointCount - car1.Value.checkPointCount);
+            //return (int)deltacheckPointCount;
+            int nextcheckPoint1 = (car1.Value.lastCheckPoint + 1) % SNumberOfCheckPoints;
+            int nextcheckPoint2 = (car2.Value.lastCheckPoint + 1) %SNumberOfCheckPoints;
+
+            float dist1 = (SCheckPointList[nextcheckPoint1].transform.position - car1.Key.transform.position).magnitude;
+            float dist2 = (SCheckPointList[nextcheckPoint2].transform.position - car2.Key.transform.position).magnitude;
+
+            if (car1.Value.lap > car2.Value.lap)
+                return -1;
+            else if (car1.Value.lap < car2.Value.lap)
+                return 1;
+            else
+            {
+                if(car1.Value.lastCheckPoint > car2.Value.lastCheckPoint)
+                    return -1;
+                if (car1.Value.lastCheckPoint < car2.Value.lastCheckPoint)
+                    return 1;
+                else
+                {
+                    if (dist1 < dist2)
+                        return -1;
+                    else if (dist1 > dist2)
+                        return 1;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        SCheckPointList = CheckPointList.ToArray();
+        SNumberOfCheckPoints = CheckPointList.Count;
+        CarController[] cars = _carContainer.GetComponentsInChildren<CarController>(true);
+        foreach (CarController car in cars)
+        {
+            _carPositions[car] = new PositionData();
+        }
+
+        InvokeRepeating("getRaceRanking", 0.0f, 0.005f);
+    }
+
+    public void getRaceRanking()
+    {
+
+        _carPosList = _carPositions.ToList<KeyValuePair<CarController, PositionData>>();
+        _carPosList.Sort(new CustomComparer());
+        int i = 1;
+        foreach (KeyValuePair<CarController, PositionData> car in _carPosList)
+        {
+            car.Value.position = i++;
+        }
+    }
+
+    public CarController getFirstCar()
+    {
+        return _carPosList.First().Key;
+    }
+
+    public CarController getLastCar()
+    {
+        return _carPosList.Last().Key;
+    }
+
+    public CarController getNext(CarController car)
+    {
+
+        CarController itemToReturn = null;
+
+        PositionData carPosData = null;
+        carPosData = _carPositions[car];
+        if (carPosData == null || carPosData.position >= _carPosList.Count)
+            return null;
+
+        itemToReturn = _carPosList[carPosData.position].Key;
+
+        return itemToReturn;
+    }
+
+    public void CheckpointTriggered(CarController car, int checkPointIndex)
+    {
+
+        PositionData carData = _carPositions[car];
+
+        if (!_finished)
+        {
+            if (checkPointIndex == 0)
+            {
+                if (carData.lastCheckPoint == CheckPointList.Count() - 1)
+                {
+                    carData.lastCheckPoint = checkPointIndex;
+                    carData.lap += 1;
+                    //Debug.Log(car.name + " lap " + carData.lap);
+                    if (IsPlayer(car))
+                    {
+                        GetComponent<RaceManager>().Announce("Tour " + (carData.lap + 1).ToString());
+                    }
+
+                    if (carData.lap >= _totalLaps)
+                    {
+                        _finished = true;
+                        GetComponent<RaceManager>().EndRace(car.name.ToLower());
+                    }
+                    if (carData.lastCheckPoint == -1 )
+                        carData.lastCheckPoint = 0;
+                }
+            }
+            else if (carData.lastCheckPoint < checkPointIndex - 1) //Checkpoints must be hit in order
+            {
+                carData.lastCheckPoint = checkPointIndex;
+            }
+        }
+
+        _carPositions[car] = carData;
+
+    }
+
+    bool IsPlayer(CarController car)
+    {
+        return car.GetComponent<CarUserControlMP>() != null;
+    }
 }
